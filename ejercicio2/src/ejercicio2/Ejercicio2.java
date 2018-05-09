3129f2d3dc565c4bfdad4c7a92d12666e61b457d
7mo tecnica <7mo tecnica@192.168.0.154>
2018-05-09 08:15:31 -0300

    Ejercicio 2 Tp

---
+++ ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,113 @@
+package ejercicio2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+/**
+ *
+ * @author Casa
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        operacion("texto.txt");
+        
+        File oldfile =new File("texto.txt"); //nombre archivo viejo
+        File newfile =new File("texto.txt.bkp"); //nombre archivo nuevo
+		
+        if(oldfile.renameTo(newfile)){
+            System.out.println("Archivo generado exitosamente!");
+        }else{
+            System.out.println("Problema al generar archivo!");
+        }
+
+        oldfile =new File("temp.txt"); //nombre archivo viejo
+        newfile =new File("texto.txt"); //nombre archivo nuevo
+		
+        if(oldfile.renameTo(newfile)){
+            System.out.println("Archivo generado exitosamente!");
+        }else{
+            System.out.println("Problema al generar archivo!");
+        }   
+        
+    }
+
+    public static void appendear_archivo(String texto, String line)
+    {
+        FileWriter fichero = null;
+        Writer pw = null;
+        //fichero = new FileWriter(texto);
+        //pw = new PrintWriter(fichero);
+        try(PrintWriter output = new PrintWriter(new FileWriter(texto,true)))
+        {
+            output.printf("%s\r\n", line);
+        }
+        catch (Exception e) {}
+        try {
+            if (null != fichero)
+                fichero.close();
+        } catch (IOException e2) {
+            System.out.println(e2.toString());
+        }
+    }
+    
+    public static void operacion(String texto) {
+       File archivo = null;
+       FileReader fr = null;
+       BufferedReader br = null;
+       String textoTemp = "temp.txt";
+       
+       try {
+          // Apertura del fichero y creacion de BufferedReader para poder
+          // hacer una lectura comoda (disponer del metodo readLine()).
+          archivo = new File (texto);
+          fr = new FileReader (archivo);
+          br = new BufferedReader(fr);
+          
+          // Lectura del fichero
+          String linea;
+          int pos;
+          while((linea=br.readLine())!=null){
+            //System.out.println(linea);
+             
+            pos = 0;
+            boolean capitalize = true;
+            StringBuilder sb = new StringBuilder(linea);
+            while (pos < sb.length()) {
+                if (sb.charAt(pos) == '.') {
+                   capitalize = true;
+                } else if (capitalize && !Character.isWhitespace(sb.charAt(pos))) {
+                   sb.setCharAt(pos, Character.toUpperCase(sb.charAt(pos)));
+                   capitalize = false;
+                }
+                pos++;
+            }
+            
+            appendear_archivo(textoTemp,sb.toString());
+                      
+          }
+                  
+       }
+       catch(IOException e){
+           System.out.println(e.toString());
+       }finally{
+          try{                    
+             if( null != fr ){   
+                fr.close();     
+             }                  
+          }catch (IOException e2){ 
+              System.out.println(e2.toString());
+          }
+       }
+    }
+}
